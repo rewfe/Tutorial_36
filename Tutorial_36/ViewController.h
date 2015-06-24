58fa2190bcdd66407b2ebb8dd15145982552a8d3
rewfe <qrewfe@yandex.ru>
2015-06-24 04:23:23 +0300

    UIActionSheet

--- Tutorial_36/ViewController.h
+++ Tutorial_36/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate>
 
+- (IBAction)showNormalActionSheet:(id)sender;
+- (IBAction)showDeleteConfirmation:(id)sender;
+
+- (IBAction)showColorsActionSheet:(id)sender;
 
 @end
 
--- Tutorial_36/ViewController.m
+++ Tutorial_36/ViewController.m
@@ -24,4 +24,62 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showNormalActionSheet:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"What do you want to do with the file?"
+                                                             delegate:self
+                                                    cancelButtonTitle:@"Cancel"
+                                               destructiveButtonTitle:@"Delete it"
+                                                    otherButtonTitles:@"Copy", @"Move", @"Duplicate", nil];
+    [actionSheet showInView:self.view];
+    actionSheet.tag = 100;
+}
+
+- (IBAction)showDeleteConfirmation:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Really delete the selected contact?"
+                                                             delegate:self
+                                                    cancelButtonTitle:@"No, I changed my mind"
+                                               destructiveButtonTitle:@"Yes, delete it"
+                                                    otherButtonTitles:nil];
+    
+    [actionSheet showInView:self.view];
+    actionSheet.tag = 200;
+}
+
+- (IBAction)showColorsActionSheet:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Pick a color:"
+                                                             delegate:self
+                                                    cancelButtonTitle:nil
+                                               destructiveButtonTitle:nil
+                                                    otherButtonTitles:@"Red", @"Green", @"Blue", @"Orange", @"Purple", nil];
+    
+    [actionSheet showInView:self.view];
+    actionSheet.tag = 300;
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (actionSheet.tag == 100) {
+        NSLog(@"The Normal action sheet.");
+    }
+    else if (actionSheet.tag == 200){
+        NSLog(@"The Delete confirmation action sheet.");
+    }
+    else{
+        NSLog(@"The Color selection action sheet.");
+    }
+    
+    NSLog(@"Index = %ld - Title = %@", (long)buttonIndex, [actionSheet buttonTitleAtIndex:buttonIndex]);
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    if (actionSheet.tag == 300) {
+        NSLog(@"From didDismissWithButtonIndex - Selected Color: %@", [actionSheet buttonTitleAtIndex:buttonIndex]);
+    }
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet willDismissWithButtonIndex:(NSInteger)buttonIndex{
+    if (actionSheet.tag == 300) {
+        NSLog(@"From willDismissWithButtonIndex - Selected Color: %@", [actionSheet buttonTitleAtIndex:buttonIndex]);
+    }
+}
+
 @end
